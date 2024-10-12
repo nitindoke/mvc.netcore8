e33782d8d30d7f9855e6bdb347592c2625150e2f
nitin doke <nitin123.doke@gmail.com>
2024-10-12 06:47:50 +0530

    code checked in for mvc8 app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NETCoreMVCDockerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
